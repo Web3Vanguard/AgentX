1b9570cfb97ffcab2c382c408f152b0103b18681
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-11-04 19:35:02 +0100

    chore: implement logrus for logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,16 @@
 package main
 
 import (
-	// "log"
-	// "github.com/jieliu2000/anyi"
-	// "fmt"
+	
 
 	"github.com/joho/godotenv"
-	// "context"
-	"fmt"
-	"log"
 	"flag"
-	// "log"
+	"log"
+	"github.com/sirupsen/logrus"
 
 	"github.com/jieliu2000/anyi"
 	"github.com/shaaibu7/AgentX/executors"
-	// "github.com/jieliu2000/anyi/flow"
 )
-
-
 
 func init() {
 	blockStepWrapper := executors.NewGetBlockStepWrapper()
@@ -25,9 +18,6 @@ func init() {
 	chainIdStepWrapper := executors.NewGetChainIdStepWrapper()
 	deployERC20TokenWrapper := executors.NewDeployERC20TokenStepWrapper()
 	deployNFTTokenWrapper := executors.NewDeployNFTTokenStepWrapper()
-
-
-
 
 	anyi.RegisterExecutor("get_current_block_number", blockStepWrapper)
 
@@ -39,10 +29,7 @@ func init() {
 
 	anyi.RegisterExecutor("deploy_nft_token", deployNFTTokenWrapper)
 
-
-
 }
-
 
 func main() {
 	configPath := flag.String("name", "./config.yaml", "config path string")
@@ -57,7 +44,6 @@ func main() {
 	flag.Parse()
 	configFilePath := "./" + *configPath
 
-
 	err = anyi.ConfigFromFile(configFilePath)
 
 	if err != nil {
@@ -66,102 +52,105 @@ func main() {
 
 	flow, err := anyi.GetFlow("agentic_flow")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("AI agent interaction without external interactions...")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
+
 	}
-
-	result, err := flow.RunWithInput("AI agent interaction without external interactions...")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	} 
-
-	log.Printf("Result: %s", result.Text)
-
-	fmt.Println("Still processing....")
 
 	flow, err = anyi.GetFlow("agentic_flow_get_block_number")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("Fetch the current block number of the somnia blockchain")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
 	}
-
-	result, err = flow.RunWithInput("Fetch the current block number of the somnia blockchain")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	} 
-
-	log.Printf("Result: %s", result.Text)
-
-	fmt.Println("Still processing....")
 
 	flow, err = anyi.GetFlow("agentic_flow_price")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("Fetch the current gas price of the somnia blockchain")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
 	}
-
-
-	result, err = flow.RunWithInput("Fetch the current gas price of the somnia blockchain")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	}
-
-
-	log.Printf("Result: %s", result.Text)
-
-
-	fmt.Println("Still processing....")
 
 	flow, err = anyi.GetFlow("agentic_flow_chain_id")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("Fetch the chain id of the somnia blockchain")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
 	}
-
-
-	result, err = flow.RunWithInput("Fetch the chain id of the somnia blockchain")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	}
-
-
-	log.Printf("Result: %s", result.Text)
-
-
-	fmt.Println("Still processing....")
 
 	flow, err = anyi.GetFlow("agentic_flow_deploy_erc20")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("Deploy an ERC20 token on the somnia blockchain")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
 	}
-
-
-	result, err = flow.RunWithInput("Deploy an ERC20 token on the somnia blockchain")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	}
-
-
-	log.Printf("Result: %s", result.Text)
-
-
-	fmt.Println("Still processing....")
 
 	flow, err = anyi.GetFlow("agentic_flow_deploy_nft")
 	if err != nil {
-		log.Fatalf("Failed to get flow: %v", err)
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Info("Failed to get flow from config file, check the config")
+	} else {
+		result, err := flow.RunWithInput("Deploy an NFT token on the somnia blockchain")
+
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"error": err,
+			}).Warn("Flow execution failed")
+		}
+
+		log.Printf("Result: %s", result.Text)
 	}
-
-
-	result, err = flow.RunWithInput("Deploy an NFT token on the somnia blockchain")
-
-	if err != nil {
-		log.Fatalf("Flow execution failed: %v", err)
-	}
-
-
-	log.Printf("Result: %s", result.Text)
-
 
 }
